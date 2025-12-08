fa22eef9be2b57db5540f918b199240411bea183
FALOSAS <falosas@alumno.upv.es@ldsic-vdi07.upvnet.upv.es>
2025-12-08 19:13:44 +0100

    Codigos problema dia 1

--- AOC1-2.c
+++ AOC1-2.c
@@ -10,7 +10,7 @@ void solve()
         std::cerr << "No se pudo abrir 'datosAOC1.txt'.\n";
         return;
     }
-
+    //Usamos un numero enorme para usar solo numeros positivos
     long long pos = 1000000000000LL + 50; 
     long long zeros = 0;
     char dir;
@@ -23,11 +23,16 @@ void solve()
         if (dir == 'R') 
 	{
             pos += pasos;
+	    //Cuenta cuántos múltiplos de 100 hay en (old, new]
             zeros += (pos / 100) - (old_pos / 100);
         } 
         else if (dir == 'L')
        	{
             pos -= pasos;
+	    //Cuenta multiplos entre variables
+	    //Restamos 1 para evitar que haga cuentas extra en el total
+	    //si partimos del 0 y lo incluimos en el nuevo por si vuelve
+	    //a pararse en 0.
             zeros += ((old_pos - 1) / 100) - ((pos - 1) / 100);
         }
     }
--- AOC1.c
+++ AOC1.c
@@ -13,20 +13,25 @@ void solve()
         return;
     }
 
-    int dial_pos = 50;       
-    int zeros = 0;  
+    int dial_pos = 50;  //El problema indica que el dial empieza en 50.     
+    int zeros = 0;  //Contador de paradas en 0
     char direction;
     int pasos;
 
+    //Lectura del archivo que primero obtiene el caracter de la direccion
+    //y luego el numero entero omitiendo espacios
     while (file >> direction >> pasos)
     {
-        
+        //Opcion giro a derecha
         if (direction == 'R')
        	{
             dial_pos = (dial_pos + pasos) % 100;
         } 
+	//Opcion giro a izquierda
         else if (direction == 'L')
        	{
+	    //Si el resultado es negativo devuelve un resto negativo.
+            //Se ajustar para que el ciclo sea entre 0-99.
             dial_pos = (dial_pos - pasos);
             dial_pos = (dial_pos % 100); 
             if (dial_pos < 0) 
@@ -35,7 +40,8 @@ void solve()
             }
         }
 
-        if (dial_pos == 0) {
+        if (dial_pos == 0) 
+	{
             zeros++;
         }
     }
